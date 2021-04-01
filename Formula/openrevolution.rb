283c38e80d60e290615e996346c142190255eec8
FreeApp2014 <freeapp2014@ya.ru>
2021-04-01 22:41:29 +0300

    Update openrevolution.rb

--- Formula/openrevolution.rb
+++ Formula/openrevolution.rb
@@ -3,7 +3,7 @@ class Openrevolution < Formula
   homepage "https://github.com/ic-scm/openrevolution"
   url "https://github.com/ic-scm/openrevolution/archive/v2.6.0.tar.gz"
   version "2.6.0"
-  sha256 "dbde1e1f62f4f1da61653dc271446d0f4e9e0130cec005723af3052c7ddc774"
+  sha256 "7dbde1e1f62f4f1da61653dc271446d0f4e9e0130cec005723af3052c7ddc774"
   license "GPLv3"
 
   depends_on "rt-audio" => :build
